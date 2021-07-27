3c0579c8710e6247b72758716060b073055d4dcf
thejunaidiqbal1 <thejunaidiqbal@outlook.com>
2021-07-27 11:46:57 +0500

    deploy

--- db/migrate/20210715082017_create_posts.rb
+++ db/migrate/20210715082017_create_posts.rb
@@ -1,13 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def up
     change_table :posts do |t|
-      t.string :name
-      t.string :title
-      t.text :content
+      # t.string :name
+      # t.string :title
+      # t.text :content
       #t.datetime :publish_at
       
       t.remove :publish_at
-      t.timestamps
+      #t.timestamps
     end
     add_column :posts, :published, :boolean, :default => false
     
