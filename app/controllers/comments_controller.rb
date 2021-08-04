9d316daaef9bcced4a33fdd068e3f3345eecfd62
thejunaidiqbal1 <thejunaidiqbal@outlook.com>
2021-08-04 12:01:33 +0500

    resolve some issues

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[show edit update destroy]
-  before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
+  before_action :set_comment, only: %i[edit update destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
 
 
